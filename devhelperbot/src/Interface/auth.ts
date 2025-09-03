9bafbc7fadb500bc3914ff77519a54388c271776
YehuditSabag <sy3134074@gmail.com>
2025-09-03 12:05:00 +0300

    create Auth

---
+++ devhelperbot/src/Interface/auth.ts
@@ -0,0 +1,24 @@
+export interface User {
+  id: string;
+  email: string;
+  name: string;
+}
+
+export interface AuthContextType {
+  user: User | null;
+  login: (email: string, password: string) => Promise<void>;
+  register: (name: string, email: string, password: string) => Promise<void>;
+  logout: () => void;
+  loading: boolean;
+}
+
+export interface LoginData {
+  email: string;
+  password: string;
+}
+
+export interface RegisterData {
+  name: string;
+  email: string;
+  password: string;
+}
